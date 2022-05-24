35a878cc9d43af25e2918f100183a37afdad4fbf
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-05-24 15:29:06 +0700

    use livedata for add controll

--- app/src/main/java/com/example/luyentapthread_bai04_taihinh/MainActivity.java
+++ app/src/main/java/com/example/luyentapthread_bai04_taihinh/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.luyentapthread_bai04_taihinh.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding mainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addControl();
+
+    }
+
+    private void addControl() {
+        mainBinding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
     }
 }

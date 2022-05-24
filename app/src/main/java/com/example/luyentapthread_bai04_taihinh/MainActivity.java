db1bfc7b9f76c2c0feb0c831d398e742163d2d21
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-05-24 15:25:16 +0700

    create base

---
+++ app/src/main/java/com/example/luyentapthread_bai04_taihinh/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.luyentapthread_bai04_taihinh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
